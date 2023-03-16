d1e315254000b7c39d7ed65c0516246b1734a6b9
ShariaHabib <59577264+ShariaHabib@users.noreply.github.com>
2023-03-17 03:20:34 +0600

    basics of java

---
+++ basic_arithmetic.java
@@ -0,0 +1,19 @@
+
+class basic_arithmetic {
+
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 15;
+        int addition = x + y;
+        int subtraction = x - y;
+        int division = x / y;
+        int multiplication = x * y;
+
+        System.out.println("Result of addition " + addition);
+        System.out.println("Result of subtraction " + subtraction);
+        System.out.println("Result of division " + division);
+        System.out.println("Result of multiplication " + multiplication);
+
+    }
+
+}
