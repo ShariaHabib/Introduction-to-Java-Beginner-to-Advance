ac210f0cd64bb6c4c9f5eda1ba0da830977ec746
ShariaHabib <59577264+ShariaHabib@users.noreply.github.com>
2023-03-25 01:16:27 +0600

    updated example codes

---
+++ chapter-2/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; x = x + 1)
+            System.out.println("This is x: " + x);
+    }
+}
