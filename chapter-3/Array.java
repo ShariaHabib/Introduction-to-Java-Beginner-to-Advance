13b6aaf8f50e19c2e0774942112a521a2652bf9b
ShariaHabib <59577264+ShariaHabib@users.noreply.github.com>
2023-03-25 01:15:09 +0600

    updated example codes

--- chapter-3/Array.java
+++ chapter-3/Array.java
@@ -1,20 +1,20 @@
 public class Array {
+
     public static void main(String[] args) {
-        int[] array = new int[10];
-        for (int i = 0; i < 10; i++) {
-            array[i] = i + 1;// assigning the value to each index.
-        }
-        System.out.println("Printing the array: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println("");
-
-        char[] arr = { 'S', 'H', 'A', 'R', 'I', 'A' };
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-        }
-        System.out.println("");
-
+        int month_days[];
+        month_days = new int[12];
+        month_days[0] = 31;
+        month_days[1] = 28;
+        month_days[2] = 31;
+        month_days[3] = 30;
+        month_days[4] = 31;
+        month_days[5] = 30;
+        month_days[6] = 31;
+        month_days[7] = 31;
+        month_days[8] = 30;
+        month_days[9] = 31;
+        month_days[10] = 30;
+        month_days[10] = 31;
+        System.out.println("April has " + month_days[3] + " days.");
     }
 }
